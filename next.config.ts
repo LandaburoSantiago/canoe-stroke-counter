b4b194148aaad8008eb669facea8e953b99ab525
santiagolandaburo <santiagol@bombieri.com.ar>
2025-02-04 22:50:05 -0300

    gitignore y next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
