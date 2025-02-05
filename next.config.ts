98c31da6f6d3de4b56a4c57732cd0ab5e9218855
santiagolandaburo <santiagol@bombieri.com.ar>
2025-02-04 22:45:45 -0300

    firstcommit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = withPWA({
+  ...nextConfig,
+});
