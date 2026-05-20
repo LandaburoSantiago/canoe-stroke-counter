c0ab7396ef2c2f0749bccb7b57d5cbbf6ae422be
LandaburoSantiago <santilandaburo73@gmail.com>
2026-05-20 12:25:07 -0300

    feat: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ServiceWorker from "./components/ServiceWorker";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleTagManager gtmId="GTM-5T77TCGN" />
         <ServiceWorker />
         <div className="flex flex-col items-center justify-center h-screen gap-4 px-4 py-16">
           <h1 className="text-2xl font-bold">Cuenta Paladas</h1>
